0c3a9528d1d2891eee6384218a67b9b665344b2e
Ewan Breakey <ebreakey@gmail.com>
2022-12-06 16:52:34 +1100

    Implement solution for day6

---
+++ day6/src/main.rs
@@ -0,0 +1,17 @@
+use std::{collections::HashSet, fs::read_to_string};
+
+fn main() {
+    let input = read_to_string("./input.txt").unwrap();
+    println!("[PT1] {}", find_packet_start(input.chars(), 4).unwrap());
+    println!("[PT2] {}", find_packet_start(input.chars(), 14).unwrap());
+}
+
+fn find_packet_start(stream: impl Iterator<Item = char>, buffer_size: usize) -> Option<usize> {
+    stream
+        .collect::<Vec<_>>()
+        .windows(buffer_size)
+        .enumerate()
+        .take_while(|(_, window)| HashSet::<&char>::from_iter(window.iter()).len() < buffer_size)
+        .last()
+        .map(|(i, _)| i + buffer_size + 1)
+}
